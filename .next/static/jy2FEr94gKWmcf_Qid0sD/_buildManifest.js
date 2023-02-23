25c2d9ad35c2b2b333bf1cfd8a2cf105d4ef50c1
nghthwkmusic <matt@doublediamond.io>
2023-02-23 15:29:32 -0700

    Change Improvements

---
+++ .next/static/jy2FEr94gKWmcf_Qid0sD/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/675-f1007a7892657f85.js","static/chunks/pages/index-5938256ba2544d52.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
